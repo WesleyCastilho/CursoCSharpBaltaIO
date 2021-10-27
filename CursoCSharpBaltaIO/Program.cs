4d10bd441c2f8593f7b02ed7fd208b9e578c3d08
Wesley <wesley.castilho@cbyk.com.br>
2021-10-27 14:48:21 -0300

    Implemented Division feature and application menu

--- CursoCSharpBaltaIO/Program.cs
+++ CursoCSharpBaltaIO/Program.cs
@@ -6,7 +6,35 @@ namespace CursoCSharpBaltaIO
     {
         static void Main(string[] args)
         {
-            Soma();
+            Menu();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Selecione uma opção:");
+            short escolha = short.Parse(Console.ReadLine());
+            Console.WriteLine(escolha);
+
+            switch (escolha)
+            {
+                case 1: Soma();
+                    break;
+                case 2: Subtracao();
+                    break;
+                case 3: Multiplicacao();
+                    break;
+                case 4: Divisao();
+                    break;
+            }
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            
         }
 
         static void Soma()
@@ -43,6 +71,17 @@ namespace CursoCSharpBaltaIO
             Console.WriteLine();
             float multiplica = v1 * v2;
             Console.WriteLine($"O resultado é: {multiplica}");
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo Valor");
+            float v2 = float.Parse(Console.ReadLine());
+            float divisao = v1 / v2;
+            Console.WriteLine($"O Resultado é: {divisao}");
         }
     }
 }
