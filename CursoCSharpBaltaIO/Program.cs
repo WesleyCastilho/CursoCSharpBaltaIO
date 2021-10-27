b866590052c97f7706708476f4833b0fbc652b7b
Wesley <wesley.castilho@cbyk.com.br>
2021-10-27 15:06:55 -0300

    Finished Calculator

--- CursoCSharpBaltaIO/Program.cs
+++ CursoCSharpBaltaIO/Program.cs
@@ -7,7 +7,6 @@ namespace CursoCSharpBaltaIO
         static void Main(string[] args)
         {
             Menu();
-            
         }
 
         static void Menu()
@@ -18,6 +17,7 @@ namespace CursoCSharpBaltaIO
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Multiplicação");
             Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair");
             Console.WriteLine();
             Console.WriteLine("---------------------");
             Console.WriteLine("Selecione uma opção:");
@@ -34,14 +34,21 @@ namespace CursoCSharpBaltaIO
                     break;
                 case 4: Divisao();
                     break;
+                case 5: System.Environment.Exit(0);
+                    break;
                 default: Menu();
                     break;
             }
         }
 
+        static void Banner(string nome)
+        {
+            Console.WriteLine($"Você selecionou a função: {nome}");
+        }
         static void Adicao()
         {
             Console.Clear();
+            Banner("Adição");
             Console.WriteLine("Primeiro valor:");
             float v1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Segundo valor:");
@@ -56,6 +63,7 @@ namespace CursoCSharpBaltaIO
         static void Subtracao()
         {
             Console.Clear();
+            Banner("Subtração");
             Console.WriteLine("Primeiro valor:");
             float v1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Segundo valor:");
@@ -70,6 +78,7 @@ namespace CursoCSharpBaltaIO
         static void Multiplicacao()
         {
             Console.Clear();
+            Banner("Multiplicação");
             Console.WriteLine("Primeiro Valor:");
             float v1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Segundo Valor:");
@@ -84,10 +93,17 @@ namespace CursoCSharpBaltaIO
         static void Divisao()
         {
             Console.Clear();
+            Banner("Divisão");
             Console.WriteLine("Primeiro Valor:");
             float v1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Segundo Valor");
             float v2 = float.Parse(Console.ReadLine());
+            if (v2.Equals(0))
+            {
+                Console.WriteLine("Não é permitido dividir por zero, ainda :)");
+                Console.ReadKey();
+                Menu();
+            }
             float divisao = v1 / v2;
             Console.WriteLine($"O Resultado é: {divisao}");
             Console.ReadKey();
